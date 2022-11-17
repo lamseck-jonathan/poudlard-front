919209416847afd47ae33ac2c757b5c3477e1b4d
Tafita Raza <tatita.raza@outlook.com>
2022-11-17 21:40:16 +0300

    create authLayout

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-light">
+    <q-header style="background: transparent">
+      <q-tabs
+        dense
+        inline-label
+        class="text-primary shadow-2 q-px-lg"
+        :align="'left'"
+        style="box-shadow: none"
+      >
+        <q-tab
+          name="mails"
+          label="Connexion"
+          :ripple="false"
+          @click.prevent="goTo('login')"
+        />
+        <q-tab
+          name="alarms"
+          label="Inscription"
+          :ripple="false"
+          @click.prevent="goTo('signIn')"
+        />
+      </q-tabs>
+    </q-header>
+
+    <!-- content area -->
+    <q-page-container>
+      <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+/**
+ * Navigate
+ * @param { string } routeName
+ */
+function goTo(routeName: string) {
+  router.push({ name: routeName });
+}
+</script>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
