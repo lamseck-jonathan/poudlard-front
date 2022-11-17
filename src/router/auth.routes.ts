e2e1b8f67979b382bb9b6fe826e5d0e7f5c56721
Tafita Raza <tatita.raza@outlook.com>
2022-11-17 21:41:01 +0300

    create auth.routes.ts

---
+++ src/router/auth.routes.ts
@@ -0,0 +1,22 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/auth',
+    component: () => import('layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import('src/pages/auth/LoginPage.vue'),
+      },
+      {
+        path: 'signin',
+        name: 'signIn',
+        component: () => import('src/pages/auth/SignInPage.vue'),
+      },
+    ],
+  },
+];
+
+export default routes;
