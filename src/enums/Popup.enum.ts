71a754b7ed4087d4ed9356b5262ae16e1b00a943
Tafita Raza <tafita.raza@outlook.com>
2022-11-22 23:50:49 +0300

    create popup composable

---
+++ src/enums/Popup.enum.ts
@@ -0,0 +1,14 @@
+export enum PopupButton {
+  OK = 'OK',
+  CANCEL = 'CANCEL',
+  YES = 'YES',
+  NO = 'NO',
+  DOWNLOAD = 'DOWNLOAD',
+}
+
+export enum PopupType {
+  INFO = 'INFO',
+  WARNING = 'WARNING',
+  ERROR = 'ERROR',
+  CONFIRMATION = 'CONFIRMATION',
+}
