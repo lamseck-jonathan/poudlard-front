3f2e05c776c2c26026c47c5b1925b53f4f8aa203
Tafita Raza <tafita.raza@outlook.com>
2022-11-22 23:51:38 +0300

    add contextMenu to Datagrid

--- src/components/CustomDatagrid.vue
+++ src/components/CustomDatagrid.vue
@@ -85,6 +85,15 @@
           :props="props"
           :style="styleObject"
         >
+          <base-context-menu
+            v-if="useContextMenu"
+            :items="contextMenuItems"
+            @click:item="
+              (itemMenu) =>
+                emit('click:contextItem', { itemMenu, data: props.row })
+            "
+          />
+
           <span :id="props.row?.templateId">
             <template v-if="typeof props.row[column.name] === 'boolean'">
               <q-badge
@@ -164,13 +173,15 @@
 </template>
 
 <script lang="ts" setup>
+import BaseContextMenu from './BaseContextMenu.vue';
 import { DatagridColumns } from 'src/model/DatagridColumns.interface';
 import { useMainLayoutStore } from 'src/stores/main-layout-store';
 import { computed, PropType, ref } from 'vue';
+import { ItemContextMenu } from 'src/model/ItemContextMenu.interface';
 
 const mainLayoutStore = useMainLayoutStore();
 
-const emit = defineEmits(['click:add', 'click:delete']);
+const emit = defineEmits(['click:add', 'click:delete', 'click:contextItem']);
 
 const filter = ref<string>('');
 const isCompactMode = ref<boolean>(false);
@@ -237,6 +248,33 @@ const props = defineProps({
   deleteBtnColumnLabel: {
     type: String,
     required: false,
+  },
+  useContextMenu: {
+    type: Boolean,
+    default: false,
+  },
+  contextMenuItems: {
+    type: Array as PropType<ItemContextMenu[]>,
+    default: () => [
+      {
+        label: 'Voir',
+        event: 'voir',
+        appendIcon: 'mdi-eye-outline',
+        isDisabled: false,
+      },
+      {
+        label: 'Modifier',
+        event: 'modifier',
+        appendIcon: 'mdi-file-edit-outline',
+        isDisabled: false,
+      },
+      {
+        label: 'Supprimer',
+        event: 'supprimer',
+        appendIcon: 'mdi-trash-can-outline',
+        isDisabled: false,
+      },
+    ],
   },
 });
 
