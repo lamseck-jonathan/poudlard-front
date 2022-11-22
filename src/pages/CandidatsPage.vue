3fa8029de6983489ae4d8b331ee69179c854766a
Tafita Raza <tafita.raza@outlook.com>
2022-11-22 20:17:35 +0300

    create Candidats page

--- src/pages/CandidatsPage.vue
+++ src/pages/CandidatsPage.vue
@@ -1,12 +1,22 @@
 <template>
-  <q-page class="flex justify-center items-center">
-    <div class="text-uppercase">Candidats page</div>
+  <q-page class="q-pt-md">
+    <custom-datagrid
+      row-key="id"
+      :columns="columns"
+      :rows="rows"
+      :action-btn="false"
+      show-search-input
+    />
   </q-page>
 </template>
 
 <script lang="ts" setup>
+import CustomDatagrid from 'src/components/CustomDatagrid.vue';
+import { Role } from 'src/enums/Role.enum';
+import { DatagridColumns } from 'src/model/DatagridColumns.interface';
+import { UserListing } from 'src/model/User.interface';
 import { useMainLayoutStore } from 'src/stores/main-layout-store';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const mainLayoutStore = useMainLayoutStore();
 
@@ -18,4 +28,86 @@ onMounted(() => {
     path: '/tests',
   });
 });
+
+const columns: DatagridColumns[] = [
+  {
+    name: 'nom',
+    required: true,
+    label: 'Nom',
+    align: 'left',
+    field: (row) => row.nom,
+    sortable: true,
+  },
+  {
+    name: 'prenom',
+    required: true,
+    label: 'Prénom(s)',
+    align: 'left',
+    field: (row) => row.prenom,
+    sortable: true,
+  },
+  {
+    name: 'role',
+    required: true,
+    label: 'Role',
+    align: 'left',
+    field: (row) => row.role,
+    sortable: true,
+  },
+  {
+    name: 'adresse',
+    required: true,
+    label: 'adresse',
+    align: 'center',
+    field: (row) => row.adresse,
+    sortable: true,
+  },
+  {
+    name: 'email',
+    required: true,
+    label: 'email',
+    align: 'center',
+    field: (row) => row.email,
+    sortable: true,
+  },
+  {
+    name: 'telephone',
+    required: true,
+    label: 'Téléphone',
+    align: 'center',
+    field: (row) => row.telephone,
+    sortable: true,
+  },
+  {
+    name: 'actif',
+    required: true,
+    label: 'Actif',
+    align: 'center',
+    field: (row) => row.actif,
+    sortable: true,
+  },
+];
+
+const rows = ref<UserListing[]>([
+  {
+    id: '3',
+    nom: 'JEAN MARC',
+    prenom: 'Leon',
+    role: Role.CANDIDAT,
+    adresse: 'Andrefanambohijanahary Antananarivo',
+    email: 'leon@email.com',
+    telephone: '0344745682',
+    actif: true,
+  },
+  {
+    id: '4',
+    nom: 'Doe',
+    prenom: 'John',
+    role: Role.CANDIDAT,
+    adresse: 'Chartre France',
+    email: 'john@email.com',
+    telephone: '+334563218654',
+    actif: false,
+  },
+]);
 </script>
