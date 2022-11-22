f624e5ac9f8206cc10506834f128134f6c6b6940
Tafita Raza <tafita.raza@outlook.com>
2022-11-22 22:41:43 +0300

    update utilisateursPage

--- src/pages/UtilisateursPage.vue
+++ src/pages/UtilisateursPage.vue
@@ -16,7 +16,7 @@ import { DatagridColumns } from 'src/model/DatagridColumns.interface';
 import { useMainLayoutStore } from 'src/stores/main-layout-store';
 import { onMounted, ref } from 'vue';
 import { UserListing } from 'src/model/User.interface';
-import { Role } from 'src/enums/Role.enum';
+import { fakeUtilisateursList } from 'src/data/utilisateurs.fake';
 
 const mainLayoutStore = useMainLayoutStore();
 
@@ -88,46 +88,5 @@ const columns: DatagridColumns[] = [
   },
 ];
 
-const rows = ref<UserListing[]>([
-  {
-    id: '1',
-    nom: 'Razafiarison',
-    prenom: 'Tafitasoa',
-    role: Role.ADMIN,
-    adresse: 'Ampopoka Golf Fianarantsoa',
-    email: 'tafita@email.com',
-    telephone: '0342821847',
-    actif: true,
-  },
-  {
-    id: '2',
-    nom: 'Rasoanambinina',
-    prenom: 'Patricia',
-    role: Role.RECRUITER,
-    adresse: 'Tanambao Fianarantsoa',
-    email: 'patricia@email.com',
-    telephone: '0349072975',
-    actif: true,
-  },
-  {
-    id: '3',
-    nom: 'JEAN MARC',
-    prenom: 'Leon',
-    role: Role.CANDIDAT,
-    adresse: 'Andrefanambohijanahary Antananarivo',
-    email: 'leon@email.com',
-    telephone: '0344745682',
-    actif: true,
-  },
-  {
-    id: '4',
-    nom: 'Doe',
-    prenom: 'John',
-    role: Role.CANDIDAT,
-    adresse: 'Chartre France',
-    email: 'john@email.com',
-    telephone: '+334563218654',
-    actif: false,
-  },
-]);
+const rows = ref<UserListing[]>([...fakeUtilisateursList]);
 </script>
