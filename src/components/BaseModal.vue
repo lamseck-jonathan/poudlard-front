ccfd89ce7dc2f0b128d6ddb42f4be39194c5a1c1
Tafita Raza <tafita.raza@outlook.com>
2022-11-24 00:00:41 +0300

    create Base Modal

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-dialog class="full-width" v-model="showModal">
+    <q-card :style="{ width: props.width, maxWidth: '1500px' }">
+      <q-card-section
+        class="row items-center q-py-md bg-blue-grey-7 text-white"
+      >
+        <div class="text-h6 text-uppercase">{{ title }}</div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+
+      <q-card-section>
+        <slot></slot>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const emit = defineEmits(['update:modelValue']);
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: String,
+    default: '900px',
+  },
+});
+
+const showModal = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(newValue) {
+    emit('update:modelValue', newValue);
+  },
+});
+</script>
