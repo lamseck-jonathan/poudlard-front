ac02cf779308c32d9604c9625ec3143b0bda0db6
Tafita Raza <tafita.raza@outlook.com>
2022-11-26 20:13:08 +0300

    create performing test page

---
+++ src/pages/interview/PerformingTestPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page>
+    <div class="row" style="height: 85vh">
+      <div
+        class="col-md-6 col-sm-12 flex justify-center q-py-md q-pl-md q-pr-sm"
+      >
+        <panel-section-test :test="currentTest" />
+      </div>
+      <div
+        class="col-md-6 col-sm-12 flex justify-center q-py-md q-pl-sm q-pr-md"
+      >
+        <panel-section-answer
+          :test="currentTest"
+          v-model:answer="answerModel"
+        />
+      </div>
+    </div>
+
+    <div class="row justify-end q-px-md">
+      <div class="col-6 q-pl-sm">
+        <q-btn
+          class="full-width"
+          label="Question Suivante"
+          color="primary"
+          @click="displayNextTest"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import PanelSectionAnswer from 'src/components/PanelSectionAnswer.vue';
+import PanelSectionTest from 'src/components/PanelSectionTest.vue';
+import { fakeBackendTestList, fakeFrontendTestList } from 'src/data/tests.fake';
+import { Test } from 'src/model/Test.interface';
+import { ReponseCandidat } from 'src/model/Reponse.interface';
+import { onBeforeMount, reactive, ref } from 'vue';
+
+const counter = ref<number>(0);
+const currentTest = ref<Test>({} as Test);
+const answerModel = ref<ReponseCandidat>({} as ReponseCandidat);
+const testList = reactive<Test[]>([
+  ...fakeFrontendTestList,
+  ...fakeBackendTestList,
+]);
+
+const reponseCandidats = reactive<ReponseCandidat[]>([]);
+
+onBeforeMount(() => {
+  initCurrentTest();
+  initAnswerModel(currentTest.value);
+});
+
+function displayNextTest() {
+  // save response
+  console.log(answerModel.value);
+
+  reponseCandidats.push(answerModel.value);
+
+  // display next
+  counter.value++;
+
+  if (counter.value < testList.length) {
+    initCurrentTest();
+    initAnswerModel(currentTest.value);
+  } else {
+    console.log('resultat : ', reponseCandidats);
+  }
+}
+
+function initCurrentTest() {
+  currentTest.value = { ...testList[counter.value] };
+}
+
+function initAnswerModel(test: Test) {
+  answerModel.value = { test };
+}
+</script>
