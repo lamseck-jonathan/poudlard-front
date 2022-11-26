5c171c51c07cb79c2f9f14a9b00d04580a7db8fb
Tafita Raza <tafita.raza@outlook.com>
2022-11-26 20:11:47 +0300

    create QCMU input

---
+++ src/components/RadioUniqueAnswer.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="column justify-center items-center q-gutter-lg" data-test="type">
+    <li class="col" v-for="item in answers" :key="item.id">
+      <q-btn
+        class="full-border q-pa-md cursor-pointer type-item"
+        style="width: 400px"
+        :class="{ active: activeOption === item.reponse }"
+        @click="activate(item)"
+        type="button"
+      >
+        {{ item.reponse }}
+      </q-btn>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { PropType, ref } from 'vue';
+import { Reponse } from '../model/Reponse.interface';
+
+const emit = defineEmits(['update:modelValue']);
+const props = defineProps({
+  answers: {
+    type: Array as PropType<Reponse[]>,
+  },
+  modelValue: {
+    type: Object as PropType<Reponse>,
+    required: true,
+  },
+});
+const activeOption = ref<string | undefined>(props?.modelValue?.reponse);
+function activate(option: Reponse) {
+  emit('update:modelValue', option);
+  activeOption.value = option.reponse;
+}
+</script>
+
+<style lang="sass">
+ul
+    list-style: none
+    padding: 0
+    margin: 0.5rem
+
+.active
+    box-shadow:0px 0px 0px 3px #1976D2 inset
+    background: #fafafa
+    border-radius: 5px
+    color:#1976D2
+</style>
