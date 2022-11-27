454f0198be107397aad529b1b6b50b838731e66c
Tafita Raza <tafita.raza@outlook.com>
2022-11-27 15:23:37 +0300

    create fiche candidat interface

---
+++ src/model/FicheCandidat.interface.ts
@@ -0,0 +1,45 @@
+import { Niveau } from 'src/enums/Niveau.enum';
+import { SituationActuelle } from 'src/enums/SituationActuelle.enum';
+import { Diplome } from './Diplome.interface';
+import { User } from './User.interface';
+
+export interface FicheCandidat extends User {
+  // information personnelle hérité de User
+  posteConvoite?: string;
+
+  // Compétences
+  competences?: Competence[];
+  anneeExperienceReelle?: number;
+  niveauAcademique: NiveauAcademique;
+  niveauFrancais: NiveauLinguistique;
+  niveauAnglais: NiveauLinguistique;
+
+  // Motivation et connaisances sur la société
+  connaisSociete?: boolean;
+  motivation?: string;
+
+  // Situation
+  situationActuelle?: SituationActuelle;
+  disponibilite?: string;
+  pretention?: string;
+}
+
+export interface Competence {
+  langage: string;
+  anneeExperience?: number;
+  niveau: Niveau;
+}
+
+export interface NiveauAcademique {
+  diplome?: Diplome;
+  date?: string;
+  option?: string;
+  institut?: string;
+  autre?: string;
+}
+
+export interface NiveauLinguistique {
+  lu?: number;
+  parle?: number;
+  ecrit?: number;
+}
