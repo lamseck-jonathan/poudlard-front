2bb4e294d60a3e7f633e704eb095d72c3d9dcf0e
Tafita Raza <tafita.raza@outlook.com>
2022-11-27 16:02:46 +0300

    create diplome enum

---
+++ src/enums/Diplome.enum.ts
@@ -0,0 +1,11 @@
+export enum Diplome {
+  BAC = 'BAC',
+  BAC1 = 'BAC +1',
+  BAC2 = 'BAC +2',
+  BAC3 = 'BAC +3',
+  BAC4 = 'BAC +4',
+  BAC5 = 'BAC +5',
+  LICENCE = 'LICENCE',
+  MASTER = 'MASTER',
+  DOCTORAT = 'DOCTORAT',
+}
