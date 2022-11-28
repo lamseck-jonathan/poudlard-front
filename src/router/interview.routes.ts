82d3afe8f0278393eb41c50e8a8f9c231c6e81de
Tafita Raza <tafita.raza@outlook.com>
2022-11-28 20:02:21 +0300

    add interview finished page

--- src/router/interview.routes.ts
+++ src/router/interview.routes.ts
@@ -7,12 +7,22 @@ const routes: RouteRecordRaw[] = [
     children: [
       {
         path: '',
-        redirect: '/interview/performing',
+        redirect: '/interview/home',
+      },
+      {
+        path: 'home',
+        name: 'interview-home',
+        component: () => import('pages/interview/HomePage.vue'),
       },
       {
         path: 'performing',
         name: 'performing-test',
         component: () => import('pages/interview/PerformingTestPage.vue'),
+      },
+      {
+        path: 'finished',
+        name: 'interview-finished',
+        component: () => import('pages/interview/FinishedPage.vue'),
       },
     ],
   },
