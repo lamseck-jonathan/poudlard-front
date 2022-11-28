5cedb039111a98e51f68938654b8d603aaf36fc0
Tafita Raza <tafita.raza@outlook.com>
2022-11-28 22:20:24 +0300

    udpate interfiew display sujet

--- src/components/InterviewDisplaySujet.vue
+++ src/components/InterviewDisplaySujet.vue
@@ -1,22 +1,27 @@
 <template>
   <q-card class="row">
     <q-card-section class="col">
+      <span class="text-weight-bold q-mb-md">Titre :</span> <br />
       {{ props.sujet.titre }}
     </q-card-section>
 
     <q-card-section class="col">
+      <span class="text-weight-bold q-mb-md">Description :</span> <br />
       {{ props.sujet.description }}
     </q-card-section>
 
     <q-card-section class="col-2">
+      <span class="text-weight-bold q-mb-md">Niveau :</span> <br />
       {{ props.sujet.niveau }}
     </q-card-section>
 
     <q-card-section class="col-1 text-center">
+      <span class="text-weight-bold q-mb-md">Points :</span> <br />
       {{ totalPoint(props.sujet.tests) }}
     </q-card-section>
 
     <q-card-section class="col-2 text-center">
+      <span class="text-weight-bold q-mb-md">Durée :</span> <br />
       {{ msToTime(totalDuree(props.sujet.tests)) }}
     </q-card-section>
   </q-card>
