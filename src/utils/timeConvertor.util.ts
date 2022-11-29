29f858fc24e6700fa55074b568444fa46aa1d3e0
Tafita Raza <tafita.raza@outlook.com>
2022-11-29 08:44:11 +0300

    cas ms to number in msToTime function

--- src/utils/timeConvertor.util.ts
+++ src/utils/timeConvertor.util.ts
@@ -2,5 +2,5 @@
  * @desc convert ms to time (ex: 10000 => 00:00:10)
  */
 export const msToTime = (ms: number): string => {
-  return String(new Date(ms).toISOString().slice(11, 19));
+  return String(new Date(Number(ms)).toISOString().slice(11, 19));
 };
